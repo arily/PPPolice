aa69e131e58151445013f892b24ebbaf029df578
arily <lbsaaaaa@gmail.com>
2019-10-30 20:23:44 +0900

    typo

--- public/javascripts/pppolice-farm.js
+++ public/javascripts/pppolice-farm.js
@@ -34,7 +34,7 @@ async function showFarm(bps, from, buff = -8, farmLimit = 10) {
 }
 async function calcAllAccountsFarm(bps, from, buff = -8, farmLimit = 10) {
 
-    await Promis.all(Object.keys(bps).map(async user => {
+    await Promise.all(Object.keys(bps).map(async user => {
         user = bps[user];
 
         let cabbageResult = await cabbageGetAccount(user, from);
