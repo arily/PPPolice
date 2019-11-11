5b746b37d84d8d2b387ac73a385910beffcb2e8d
arily <lbsaaaaa@gmail.com>
2019-11-12 00:10:43 +0900

    Update final.js

--- routes/final.js
+++ routes/final.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 
-router.get('/:from/:to', async function(req, res, next) {
+router.get('/', async function(req, res, next) {
     res.render('pppolice-farm', {
         rev: Math.random().toString(36).substring(7),
         emitEventName: 'Final',
-        dateTime: `'${req.params.from}','${req.params.to}'`
+        dateTime: `'2019-11-09','2019-11-17'`
     });
 });
 
