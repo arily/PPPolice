83fbc7643bde2688e616bc08ec6bccda5d83b9ab
arily <lbsaaaaa@gmail.com>
2019-12-16 19:26:03 +0900

    unmod NF SO on pp maps

--- public/javascripts/sorting/sotrash.js
+++ public/javascripts/sorting/sotrash.js
@@ -49,11 +49,13 @@ export default (pushed) => {
         score.result.withSO = function() {
             return this.mods.includes("SpunOut")
         }
-        if (score.result.isDeranking()) {
-        	score.result.pp = score.result.withSO() ? score.result.pp / 0.9 : score.result.pp;
-        	score.result.pp = score.result.withNF() ? score.result.pp / 0.9 : score.result.pp;
-        } else {
-            score.result.pp = ((score.result.beatmap.byShitMapper() || score.result.beatmap.isShitMap()) ? 0 : score.result.pp);
+        if ((score.result.beatmap.byShitMapper() || score.result.beatmap.isShitMap())) {
+            if (score.result.isDeranking()) {
+                score.result.pp = score.result.withSO() ? score.result.pp / 0.9 : score.result.pp;
+                score.result.pp = score.result.withNF() ? score.result.pp / 0.9 : score.result.pp;
+            } else {
+            	score.result.pp = 0
+            }
         }
         return score;
     });
