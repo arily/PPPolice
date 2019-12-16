c5923dbb9a9e04096d3d14f605cbfe26cd756ea1
arily <lbsaaaaa@gmail.com>
2019-12-16 19:23:49 +0900

    unmod NF SO

--- public/javascripts/sorting/sotrash.js
+++ public/javascripts/sorting/sotrash.js
@@ -41,9 +41,20 @@ export default (pushed) => {
             )
         }
         score.result.isDeranking = function() {
-            return this.mods.some(mod => (mod == 'NoFail' || mod == "SpunOut"));
+            return this.withNF() || this.withSO()
         }
-        score.result.pp = ((score.result.beatmap.byShitMapper() || score.result.beatmap.isShitMap()) && !score.result.isDeranking() ? 0 : score.result.pp)
+        score.result.withNF = function() {
+            return this.mods.includes("NoFail")
+        }
+        score.result.withSO = function() {
+            return this.mods.includes("SpunOut")
+        }
+        if (score.result.isDeranking()) {
+        	score.result.pp = score.result.withSO() ? score.result.pp / 0.9 : score.result.pp;
+        	score.result.pp = score.result.withNF() ? score.result.pp / 0.9 : score.result.pp;
+        } else {
+            score.result.pp = ((score.result.beatmap.byShitMapper() || score.result.beatmap.isShitMap()) ? 0 : score.result.pp);
+        }
         return score;
     });
     sortFunc(pushed);
