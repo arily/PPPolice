5f1506fdcc0c1b2a419c5723a29ddf8fb7ef5d59
arily <lbsaaaaa@gmail.com>
2020-06-13 06:52:16 +0900

    shit happens

--- database_tools/id_to_string.js
+++ database_tools/id_to_string.js
@@ -7,14 +7,14 @@ async function init() {
 
     data = data.map(user => {
         user.id = user.id.toString();
-        if( user.bp) user.bp = user.bp.map(score => {
+        if (user.bp) user.bp = user.bp.map(score => {
             score.beatmapId = score.beatmapId.toString()
             return score
         })
         return user;
     })
 
-    fs.writeFile('./json/export.convert.json', JSON.stringify(data), function(err) {
+    fs.writeFile('./json/export.convert.json', JSON.stringify(data), function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
